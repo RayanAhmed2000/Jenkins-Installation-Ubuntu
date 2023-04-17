1a4729a6d84412daac47bdfe507e33949dddd728
Rayan Ahmed <90337601+RayanAhmed2000@users.noreply.github.com>
2023-04-17 20:49:02 +0530

    commit

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,8 +1,8 @@
-sudo apt update -y
-sudo apt install git -y
+#!/bin/bash
+apt update -y
+apt install git -y
 git --version
 git clone https://github.com/RayanAhmed2000/Jenkins-Installation-Ubuntu.git
 cd Jenkins-Installation-Ubuntu/
 chmod 777 Jenkins.sh 
-sh Jenkins.sh 
-
+sh Jenkins.sh
